96f053e66c4bf71f06385690bfc805bf30391e72
HShrigma <hristostantchev@abv.bg>
2025-04-14 12:53:26 +0300

    Update Main.java - refactor bracket parsing

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-public class Main{
-    public static void main(String[] args) {        
-        //test out individual parsing
+public class Main {
+    public static void main(String[] args) {
+        // test out individual parsing
         String squareBrackets = "[SampleSquareBracketData]";
         String parsed = GetSquareBracketData(squareBrackets);
         System.err.println("bracket data: " + squareBrackets);
@@ -11,26 +11,25 @@ public class Main{
         System.err.println("bracket data: " + curlyBrackets);
         System.err.println("parsed: " + parsed);
         System.err.println();
-        String quotes = "\"SampleQuotesData\""; 
+        String quotes = "\"SampleQuotesData\"";
         String numLiteral = "98";
         String boolLiteral = "true";
 
     }
 
-    public static String GetSquareBracketData(String input){
-        StringBuilder rStringBuilder = new StringBuilder();
-        
-        for (int i = 0; i < input.length(); i++) {
-            if(input.charAt(i) != '[' && input.charAt(i) != ']' ) 
-                rStringBuilder.append(input.charAt(i));
-        }
-        return rStringBuilder.toString();
+    public static String GetSquareBracketData(String input) {
+        return GetStringExcludeTwoChars(input, '[',  ']');
     }
-    public static String GetCurlyBracketData(String input){
+
+    public static String GetCurlyBracketData(String input) {
+        return GetStringExcludeTwoChars(input, '{', '}');
+    }
+
+    public static String GetStringExcludeTwoChars(String input, char first, char second) {
         StringBuilder rStringBuilder = new StringBuilder();
-        
+
         for (int i = 0; i < input.length(); i++) {
-            if(input.charAt(i) != '{' && input.charAt(i) != '}' ) 
+            if (input.charAt(i) != first && input.charAt(i) != second)
                 rStringBuilder.append(input.charAt(i));
         }
         return rStringBuilder.toString();
